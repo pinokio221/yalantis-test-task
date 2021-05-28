7b61cecb20905e301df91a939981d3d6d462cd41
pinokio <pinokio221b@gmail.com>
2021-05-28 17:43:49 +0300

    general commit

--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </PersistGate>
-  </Provider>,
+  <HashRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+          <App />
+        
+      </PersistGate>
+    </Provider>
+  </HashRouter>,
 
   document.getElementById("root")
 );
